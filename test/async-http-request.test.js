df957a6226c3c7e6339e9942dc8877c0fe56bf65
spursy <582214816@qq.com>
2017-12-01 14:16:02 +0800

    consummate the unite test

--- test/async-http-request.test.js
+++ test/async-http-request.test.js
@@ -2,14 +2,18 @@
  * created by spursyy at 2017-11-30
  */
 //async-http-request.test.js
-var add = require('../lib/async-http-request.js');
+var httprequest = require('../lib/async-http-request.js');
+var httsprequest = require('../lib/async-https-request.js');
 var expect = require('chai').expect;
 
 //test suite
-describe('加法函数的测试', function() {
+describe('net work request fun', function() {
     //test case
 
-  it('1 加 1 应该等于 2', function() {
-    expect(add(1, 1)).to.be.equal(2);
-  });
-});
+  it('get json test', function() {
+    httsprequest.getJson("https://www.npmjs.com/search?q=request").then(  (data)=> {
+      console.log("data>>>>>" + JSON.stringify(data));
+    }, (err) => {
+      console.log("err>>>>>" + JSON.stringify(err));
+    })});
+})
